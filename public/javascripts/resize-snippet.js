94908c75061167b5c7ebecfbe65b3a8edcff7bd4
krehl <konstantin.krehl@gmail.com>
2016-09-16 18:28:49 +0200

    Resize Dirty Fix

--- public/javascripts/resize-snippet.js
+++ public/javascripts/resize-snippet.js
@@ -2,6 +2,6 @@
  * @author A. Roberto Fischer <a.robertofischer@gmail.com> on 7/10/2016
  */
 $(window).on("load resize", function() {
-    const offsetHeight = document.getElementById('navigation').offsetHeight;
-    document.getElementById('content').setAttribute("style", "height:" + (window.innerHeight - offsetHeight) + "px");
+    const offsetHeight = document.getElementById('app').offsetHeight;
+    document.getElementById('content').setAttribute("style", "height:" + (window.innerHeight - offsetHeight - 10) + "px");
 });
